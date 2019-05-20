64c69ab87b256c88176ae6f6ff65190532d1bf32
Daniel Feline <daniel.feline@yahoo.com>
2019-05-20 13:41:18 +0200

    ask2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,17 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
   def ask
   end
 
   def answer
     @question = params[:question]
-      if @question == "I'm going to work"
-        @answer = 'great'
-      elsif @question.include?('?')
-        @answer = 'silly'
-      else
-        @answer = "i don't care"
-      end
-
+    if @question == "I'm going to work"
+      @answer = 'great'
+    elsif @question.include?('?')
+      @answer = 'silly'
+    else
+      @answer = "i don't care"
+    end
   end
 end
