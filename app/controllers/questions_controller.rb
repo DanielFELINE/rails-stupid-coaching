b5b786d1d6ca0f3566bf622d241cccd5a9b507da
Daniel Feline <daniel.feline@yahoo.com>
2019-05-20 13:36:34 +0200

    ask

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+      if @question == "I'm going to work"
+        @answer = 'great'
+      elsif @question.include?('?')
+        @answer = 'silly'
+      else
+        @answer = "i don't care"
+      end
+
+  end
+end
